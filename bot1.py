845cdd0ef401239a30140dbe615d690611653d4d
mido398800 <65042891+mido398800@users.noreply.github.com>
2021-12-21 07:12:01 +0000

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -1,101 +1,70 @@
-
-
-
-
+import telebot
 import os
 import sys
-import time
-import telepot
-from http import cookies
-from telepot.loop import MessageLoop
-import pdb
-def _extract_message(update):
-    key = _find_first_key(update, [
-                                   'message',
-                                   'edited_message',
-                                   'channel_post',
-                                   'edited_channel_post',
-                                   'callback_query',
-                                   'inline_query',
-                                   'chosen_inline_result',
-                                   'shipping_query',
-                                   'pre_checkout_query',
-                                   'update_id'])
-    return key, update[key]
-def handle(msg):
+from dotenv import load_dotenv
+####################################
+AUTH_TOKEN = '2082814545:AAFAFzWgO1bxtJSwBRTPu3DpcRfm3zUWWQA' 
+ADMIN_ID   = '1136754989'
+load_dotenv()
+token = os.getenv(AUTH_TOKEN)
+bot = telebot.TeleBot(AUTH_TOKEN,parse_mode="MARKDOWN")
 
-    content_type, chat_type, chat_id = telepot.glance(msg)
-    if content_type == 'text' and msg["text"] == "HZjd9H":
-    	
-    		bot.sendMessage(chat_id, "Done Activited ")
-    		f2= open('paid.txt', 'a')
-    		f2.write(str(chat_id))
-    		f2.write("\n")
-    paid = open('paid.txt',mode='r+')
-    readpaid = paid.readlines()
-    x = str(chat_id)
-    for line in readpaid:
-    	if x in line:
-    		paid = 'paid'
+def main():
+
+    bot.polling()
     
-    if paid == 'paid':
-     
-	    sp = open("users.txt", "r")
-	    data = sp.read()
-	    limit = data.count(str(chat_id))
-	    
-	    sp.close()
-	    print(chat_id)
-	    if limit < 50 or chat_id == 1136754989 or str(chat_id) == '-1001446252611' or str(chat_id) == '-1001558652150' or str(chat_id) == '-1001702455980' or str(chat_id) == '-1001171130697':
-		    if content_type == 'text' and msg["text"] != "/start" and '/sc' in msg["text"]:
-		        # let the human know that the pdf is on its way        
-		        import requests as r
-		        a=r.get("https://t.me/"+msg["text"].replace("/sc ",'').replace('@','')).text
-		        b=r.get("https://sand65.ml/api/chaek.php?tu="+msg["text"].replace('/sc ',''))
-		    
-		 
-		       
-		    
-		      
-		        if '<meta property="og:description" content="">' not in a and msg["text"] != "/sc" and "members" in a or "subscribers" in a:
-		        	bot.sendMessage(chat_id, "preparing txt of fresh CC, pls wait 400 Second..\nUsed Credit : "+str(limit))
-		        	f = open('users.txt', 'a')
-		        	f.write(str(chat_id))
-		        	f.write("\n")
-		        		
-		        	import os
-		        	os.system('timeout 400 python3 scrape1.py '+msg["text"].replace('/sc ','')+" "+str(chat_id)+" &")
-		        	
-		        	os.system('timeout 420 python3 send.py '+msg["text"].replace('/sc @','')+" "+str(chat_id)+" &")
-		        	import time
-		
-		        	
-		
-		        else:
-		        	bot.sendMessage(chat_id, "No Group/Channel/User With this username")
-		        
-		    elif content_type == 'text' and '-' not in str(chat_id):
-		        bot.sendMessage(chat_id, "Hello!\nSend Group/Channel Username To Scrape CC Combolist For Free !\n\nBY • @STR_GUYS\nDwv • @S0OON")
-	    else:
-	    	bot.sendMessage(chat_id, "You Are Exceeded The Max Limit\n\nContact • @S0OON TO Activate New Key")
-    else:
-    	if content_type == 'text' and msg["text"] != "/start" and '-' not in str(chat_id):
-    	
-    		bot.sendMessage(chat_id, "You Are Not Activited User\n\nContact • @S0OON TO Activate Key")
-    	elif content_type == 'text' and msg["text"] == "/start":
-    	
-    		if '-' not in chat_id:
-    			bot.sendMessage(chat_id, "Welcome!\nJoin Us @STR_GUYS To Know The News\n\nBY • @S0OON ")
+@bot.message_handler()
+
+def greet(message):
+######################################################
+    print(message.text + "|" + str(message.chat.id)) # LOOP_PRINT
+######################################################
+    from_id    = message.chat.id
+    message_id = message.id
+    text       = message.text
+    first_name = message.chat.first_name
+    username   = message.chat.username
+    type       = message.chat.type
+    language   = message.from_user.language_code
+    start      = "NAME : " + first_name + "\nUSERNAME : " + username + "\nSTATUS : " + type + "\nLANGUAGE : " + str(language)
+    scraping   = "PLEASE WAIT 400 SECOND THEN IF I FOUND CC I WILL SEND IT \nTHIS BOT CREATED BY @S0OON\n[TEAM STRANGERS](https://t.me/PPJKK)"
+    
+    
+######################################################  
+    if "/start" == str(text) :                       #WELCOME_MESSAGE
+        bot.send_message(message.chat.id, start)     #WELCOME_MESSAGE
+######################################################
 
 
-# replace XXXX.. with your token
-TOKEN = '2082814545:AAFAFzWgO1bxtJSwBRTPu3DpcRfm3zUWWQA'
-bot = telepot.Bot(TOKEN)
-MessageLoop(bot, handle).run_as_thread()
-print ('Listening ...')
-# Keep the program running.
-while 1:
-	time.sleep(10)
+######################################################
 
-  
+
+
+    if "/sc @" in str(text) and text != "/sc @":
+        import requests as r
+        sc_user    = text.replace("/sc @",'')
+        is_channel = r.get("https://t.me/"+str(sc_user)).text
+        key_check  = '<meta property="og:description" content="">'
+        if key_check not in is_channel and text != "/sc" and "members" in is_channel or "subscribers" in is_channel:
+            bot.send_message(message.chat.id, scraping)
+            co_1   = 'timeout 400 python3 scrape1.py ' + str(sc_user) + " " + str(from_id) + " &"
+            cp_2   = 'timeout 420 python3 send.py ' + str(sc_user) + " " + str(from_id) + " &"
+            os.system(co_1)
+            os.system(co_2)
+            
+            
+######################################################
+
+if __name__ == '__main__':
+    main() 
+
+
+
+
+
+
+
+
+####################################
+
 
