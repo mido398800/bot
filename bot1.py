6b4abb9633e3aa8ed743983ce5bd63f037717790
mido398800 <65042891+mido398800@users.noreply.github.com>
2021-12-21 11:07:19 +0000

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -32,7 +32,7 @@ while True:
 			type       = message.chat.type
 			language   = message.from_user.language_code
 			start      = "NAME : " + first_name + "\nUSERNAME : " + username + "\nSTATUS : " + type + "\nLANGUAGE : " + str(language)
-			scraping   = "PLEASE WAIT 400 SECOND THEN IF I FOUND CC I WILL SEND IT \nTHIS BOT CREATED BY @S0OON\n[TEAM STRANGERS](https://t.me/PPJKK)"
+			scraping   = "PLEASE WAIT 400 SECOND THEN IF I FOUND CC I WILL SEND IT \nMY COMMAND IS '/sc @TARGET'\nTHIS BOT CREATED BY @S0OON\n[TEAM STRANGERS](https://t.me/PPJKK)"
 			
 			if type == "private" :
 				
