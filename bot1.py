dc2da8a931446bbb8cc07a571adb1187c03f2a13
mido398800 <65042891+mido398800@users.noreply.github.com>
2021-12-21 07:44:26 +0000

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -1,70 +1,74 @@
-import telebot
-import os
-import sys
-from dotenv import load_dotenv
-####################################
-AUTH_TOKEN = '2082814545:AAFAFzWgO1bxtJSwBRTPu3DpcRfm3zUWWQA' 
-ADMIN_ID   = '1136754989'
-load_dotenv()
-token = os.getenv(AUTH_TOKEN)
-bot = telebot.TeleBot(AUTH_TOKEN,parse_mode="MARKDOWN")
+try:
+	import telebot
+	import os
+	import sys
+	from dotenv import load_dotenv
+	####################################
+	AUTH_TOKEN = '2082814545:AAFAFzWgO1bxtJSwBRTPu3DpcRfm3zUWWQA' 
+	ADMIN_ID   = '1136754989'
+	load_dotenv()
+	token = os.getenv(AUTH_TOKEN)
+	bot = telebot.TeleBot(AUTH_TOKEN,parse_mode="MARKDOWN")
 
-def main():
+	def main():
 
-    bot.polling()
-    
-@bot.message_handler()
+		bot.polling()
+		
+	@bot.message_handler()
 
-def greet(message):
-######################################################
-    print(message.text + "|" + str(message.chat.id)) # LOOP_PRINT
-######################################################
-    from_id    = message.chat.id
-    message_id = message.id
-    text       = message.text
-    first_name = message.chat.first_name
-    username   = message.chat.username
-    type       = message.chat.type
-    language   = message.from_user.language_code
-    start      = "NAME : " + first_name + "\nUSERNAME : " + username + "\nSTATUS : " + type + "\nLANGUAGE : " + str(language)
-    scraping   = "PLEASE WAIT 400 SECOND THEN IF I FOUND CC I WILL SEND IT \nTHIS BOT CREATED BY @S0OON\n[TEAM STRANGERS](https://t.me/PPJKK)"
-    
-    
-######################################################  
-    if "/start" == str(text) :                       #WELCOME_MESSAGE
-        bot.send_message(message.chat.id, start)     #WELCOME_MESSAGE
-######################################################
-
-
-######################################################
+	def greet(message):
+	######################################################
+		print(message.text + "|" + str(message.chat.id)) # LOOP_PRINT
+	######################################################
+		from_id    = message.chat.id
+		message_id = message.id
+		text       = message.text
+		first_name = message.chat.first_name
+		username   = message.chat.username
+		type       = message.chat.type
+		language   = message.from_user.language_code
+		start      = "NAME : " + first_name + "\nUSERNAME : " + username + "\nSTATUS : " + type + "\nLANGUAGE : " + str(language)
+		scraping   = "PLEASE WAIT 400 SECOND THEN IF I FOUND CC I WILL SEND IT \nTHIS BOT CREATED BY @S0OON\n[TEAM STRANGERS](https://t.me/PPJKK)"
+		
+		
+	######################################################  
+		if "/start" == str(text) :                       #WELCOME_MESSAGE
+			bot.send_message(message.chat.id, start)     #WELCOME_MESSAGE
+	######################################################
 
 
-
-    if "/sc @" in str(text) and text != "/sc @":
-        import requests as r
-        sc_user    = text.replace("/sc @",'')
-        is_channel = r.get("https://t.me/"+str(sc_user)).text
-        key_check  = '<meta property="og:description" content="">'
-        if key_check not in is_channel and text != "/sc" and "members" in is_channel or "subscribers" in is_channel:
-            bot.send_message(message.chat.id, scraping)
-            co_1   = 'timeout 400 python3 scrape1.py ' + str(sc_user) + " " + str(from_id) + " &"
-            cp_2   = 'timeout 420 python3 send.py ' + str(sc_user) + " " + str(from_id) + " &"
-            os.system(co_1)
-            os.system(co_2)
-            
-            
-######################################################
-
-if __name__ == '__main__':
-    main() 
+	######################################################
 
 
 
+		if "/sc @" in str(text) and text != "/sc @":
+			import requests as r
+			sc_user    = text.replace("/sc @",'')
+			is_channel = r.get("https://t.me/"+str(sc_user)).text
+			key_check  = '<meta property="og:description" content="">'
+			if key_check not in is_channel and text != "/sc" and "members" in is_channel or "subscribers" in is_channel:
+				bot.send_message(message.chat.id, scraping)
+				co_1   = 'timeout 400 python3 scrape1.py ' + str(sc_user) + " " + str(from_id) + " &"
+				co_2   = 'timeout 420 python3 send.py ' + str(sc_user) + " " + str(from_id) + " &"
+				os.system(co_1)
+				os.system(co_2)
+				
+				
+	######################################################
+
+	if __name__ == '__main__':
+		main() 
 
 
 
 
 
-####################################
 
+
+
+	####################################
+
+
+except Exception:
+    pass
 
