9ae6fa7d19e8868537a46586a670e1c89f3936b0
mido398800 <65042891+mido398800@users.noreply.github.com>
2021-12-23 02:58:52 +0200

    Update bot1.py
    ADDED CH AND CH2 TO PREVENT USE WITHOUT  SUBSCRIBE

--- bot1.py
+++ bot1.py
@@ -3,6 +3,7 @@ import telebot
 import os
 import time
 import sys
+import requests as r
 from dotenv import load_dotenv
 while True:
 	try:
@@ -11,6 +12,7 @@ while True:
 		AUTH_TOKEN = '2082814545:AAFAFzWgO1bxtJSwBRTPu3DpcRfm3zUWWQA' 
 		ADMIN_ID     = '1136754989'
 		CH_USER      = 'STR_GUYS'
+		CH_USER2    = 'PPJKK'
 		load_dotenv()
 		token = os.getenv(AUTH_TOKEN)
 		bot     = telebot.TeleBot(AUTH_TOKEN,parse_mode="MARKDOWN")
@@ -23,7 +25,7 @@ while True:
 
 		def greet(message):
 		######################################################
-			print(message) # LOOP_PRINT
+			print("NEW MESSAGE : " + message.text + " FROM : " + str(message.chat.id)) # LOOP_PRINT
 		######################################################
 			from_id    = message.chat.id
 			message_id = message.id
@@ -32,14 +34,17 @@ while True:
 			username   = message.chat.username
 			type       = message.chat.type
 			language   = message.from_user.language_code
-			start      = "NAME : " + first_name + "\nUSERNAME : @" + username + "\nSTATUS : " + type + "\nMY COMMAND IS '/sc @TARGET'\nLANGUAGE : " + str(language)
+			start      = "HELLO , I CAN SCRAPE CC FROM GROUPS AND CHANNELS \nJUST SEND THE COMMAND\nLIKE : /sc @USER\nAND IF I FOUND ANY CC I WILL SEND IT IN A COMBO FILE\nI CREATED BY @S0OON"
 			scraping   = "يرجى الانتظار 400 ثانية ثم إذا وجدت CC فسأرسلها\n--------------------\nPLEASE WAIT 400 SECOND THEN IF I FOUND CC I WILL SEND IT \nMY COMMAND IS '/sc @TARGET'\nTHIS BOT CREATED BY @S0OON\n[TEAM STRANGERS](https://t.me/PPJKK)"
-			SUBSCRIBE_FIRST = "PLEASE SUBSCRIBE TO OUR CHANNEL TO USE ME\n-------------------------------\nCH » @STR_GUYS"
-			CH_SUB = r.get("https://api.telegram.org/bot" + AUTH_TOKEN + "/getChatMember?chat_id=@" + CH_USER + "&user_id="+str(from_id)).json()
-			CH_CHECK = CH_SUB['ok']
+			SUBSCRIBE_FIRST = "PLEASE SUBSCRIBE TO OUR CHANNEL TO USE ME\n-----------------------------------------------\nCH » @STR\_GUYS AND @PPJKK"
+			CH_SUB = r.get("https://api.telegram.org/bot" + AUTH_TOKEN + "/getChatMember?chat_id=@" + CH_USER + "&user_id=" + str(from_id)).json()
+			CH_SUB2 = r.get("https://api.telegram.org/bot" + AUTH_TOKEN + "/getChatMember?chat_id=@" + CH_USER2 + "&user_id=" + str(from_id)).json()
+			CH_CHECK = CH_SUB['result']["status"]
+			CH_CHECK2 = CH_SUB2['result']["status"]
+			print("STATUS : " + CH_CHECK + " STATUS2 : " + CH_CHECK2)
 
 			
-			if type == "private" and CH_CHECK == True :
+			if type == "private" and CH_CHECK == 'member' and CH_CHECK2 == 'member' :
 				
 			######################################################  
 				if "/start" == str(text) :                       #WELCOME_MESSAGE
@@ -52,7 +57,7 @@ while True:
 
 
 				if "/sc @" in str(text) and text != "/sc @":
-					import requests as r
+		
 					sc_user    = text.replace("/sc @",'')
 					is_channel = r.get("https://t.me/"+str(sc_user)).text
 					key_check  = '<meta property="og:description" content="">'
@@ -74,5 +79,6 @@ while True:
 
 		####################################
 
-	except:
+	except Exception as ex:
+		print(ex)
 		continue
